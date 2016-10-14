80d8ffde8cac5572916e420a3a36afad21ef796c
philtrep <phil.trepanier@gmail.com>
2016-10-14 14:21:40 -0400

    Fixed nginx no SSL simple

--- nginx/scripts/build-nginx.sh
+++ nginx/scripts/build-nginx.sh
@@ -8,13 +8,20 @@ for template in /etc/nginx/templates/*.template; do
     envsubst < $template > "/etc/nginx/sites-available/"$(basename $template)".conf"
 done
 
-if [ $NO_DEFAULT = "true" ]; then
+if [[ "$NO_DEFAULT" = true ]]; then
     rm /etc/nginx/sites-available/node.template.conf
     rm /etc/nginx/sites-available/node-https.template.conf
+else
+    if [[ "$WEB_SSL" = false ]]; then
+        rm /etc/nginx/sites-available/node-https.template.conf
+    fi
 fi
 
-if [ "$WEB_SSL" = "true" ] && [ "$NO_DEFAULT" = "false"]; then
-    if [ "$SELF_SIGNED" = "true" ]; then
+if [[ "$WEB_SSL" = true && "$NO_DEFAULT" = false ]]; then
+    if [[ "$SELF_SIGNED" = true ]]; then
+        echo "---------------------------------------------------------"
+        echo "NGINX: Generating certificates"
+        echo "---------------------------------------------------------"
         openssl req \
             -new \
             -newkey rsa:4096 \
@@ -22,11 +29,14 @@ if [ "$WEB_SSL" = "true" ] && [ "$NO_DEFAULT" = "false"]; then
             -nodes \
             -x509 \
             -subj "/C=FK/ST=Fake/L=Fake/O=Fake/CN=0.0.0.0" \
-            -keyout /etc/ssl/privkey.pem \
-            -out /etc/ssl/cacert.pem
+            -keyout /etc/ssl/privkey1.pem \
+            -out /etc/ssl/cert1.pem
         chown www-data:www-data /etc/ssl/cert1.pem
         chown www-data:www-data /etc/ssl/privkey1.pem
     else
+        echo "---------------------------------------------------------"
+        echo "NGINX: Using certificates in 'nodock/nginx/certs/'"
+        echo "---------------------------------------------------------"
         if [ -e /var/certs/cert1.pem ]; then
             cp /var/certs/cert1.pem /etc/ssl/cert1.pem
         fi
